dba6c5254fd1ab783dc7fc1ebd2f8c19e560dc5c
Wanderson <Wanderson>
2021-04-08 12:11:18 -0300

    primeiro commit

---
+++ PrjLoja/PrjLoja/Model/Pessoa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrjLoja.Model
+{
+    public class Pessoa
+    {
+        //Decalração dos atributos da classe pessoa:
+        public int Codigo{ get; set;}
+        public string Nome{ get; set; }
+        public string Endereco { get; set; }
+        public string Email { get; set; }
+        public string FoneRes { get; set; }
+        public string FoneCel { get; set; }
+        public string Documento { get; set; }
+        public string Sexo { get; set; }
+
+    }
+}
---
+++ PrjLoja/PrjLoja/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrjLoja
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
